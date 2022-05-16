49d26f5be74ec7ba29d71ef50d9d8b024272ac39
Alexander <a.s.romashkin@yandex.ru>
2022-05-16 23:19:14 +0300

    webpack analyzer

---
+++ src/AppOld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <component :is="buttonComponent" />
+    </div>
+</template>
+
+<script>
+import ButtonBY from "@/components/button/Button-BY"
+import ButtonIN from "@/components/button/Button-IN"
+
+export default {
+    computed: {
+        buttonComponent() {
+            return process.env.VUE_APP_COUNTRY === 'IN' ? ButtonIN : ButtonBY
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+    display: flex;
+    justify-content: center;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const COUNTRY = process.env.VUE_APP_COUNTRY
 module.exports = defineConfig({
     transpileDependencies: true,
     configureWebpack: {
         plugins: [
+            new BundleAnalyzerPlugin(),
             new webpack.NormalModuleReplacementPlugin(
                 /(.*)-COUNTRY(\.*)/,
                 function (resource) {
